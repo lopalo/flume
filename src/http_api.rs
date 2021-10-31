eafb33db902c558e4973793c05e3db45958c9d1b
Vlad Lopalo <vlad.lopalo@gmail.com>
2021-10-31 17:52:55 +0200

    Introduce messaging via multiple queues

--- src/http_api.rs
+++ src/http_api.rs
@@ -1,9 +1,11 @@
-use crate::queue::Queue;
+use crate::queue::QueueHub;
 use async_std::io::Result as IoResult;
 use serde::{Deserialize, Serialize};
-use tide::{Body, Request, Result as TResult, Server, StatusCode};
+use std::collections::HashMap;
+use tide::{Body, Request, Response, Result as TResult, Route, StatusCode};
 
-type Req = Request<Queue>;
+type Req = Request<QueueHub>;
+type Resp = TResult<Response>;
 
 #[derive(Deserialize)]
 struct PushMessage {
@@ -11,68 +13,123 @@ struct PushMessage {
 }
 
 async fn push_message(mut req: Req) -> TResult<StatusCode> {
-    let body: PushMessage = req.body_json().await?;
-    req.state().push(body.payload.into()).await;
-    Ok(StatusCode::Created)
+    use crate::queue::PushMessageResult::*;
+
+    let PushMessage { payload } = req.body_json().await?;
+    let queue_name = req.param("queue_name")?;
+    let res = req.state().push(queue_name, payload.into()).await;
+    match res {
+        Done => Ok(StatusCode::NoContent),
+        QueueDoesNotExist => Ok(StatusCode::NotFound),
+    }
 }
 
 #[derive(Serialize)]
+#[serde(tag = "status")]
 enum TakeMessage {
     Empty,
     Message { payload: String },
 }
 
-async fn take_message(req: Req) -> TResult<Body> {
-    use TakeMessage::*;
-    let response = match req.state().take().await {
-        None => Empty,
-        Some(p) => Message {
-            payload: String::from_utf8_lossy(&p).into(),
-        },
-    };
-    Body::from_json(&response)
+async fn take_message(req: Req) -> Resp {
+    use crate::queue::TakeMessageResult::*;
+
+    let queue_name = req.param("queue_name")?;
+    Ok(match req.state().take(queue_name).await {
+        QueueDoesNotExist => Response::builder(StatusCode::NotFound),
+        res => {
+            let body = match res {
+                QueueDoesNotExist => unreachable!(),
+                Message { payload } => TakeMessage::Message {
+                    payload: String::from_utf8_lossy(&payload).into(),
+                },
+                QueueIsEmpty => TakeMessage::Empty,
+            };
+
+            Response::builder(StatusCode::Ok).body(Body::from_json(&body)?)
+        }
+    }
+    .build())
 }
 
-fn make_messaging_routes(state: Queue) -> Server<Queue> {
-    let mut api = tide::with_state(state);
-    api.at("/push").post(push_message);
-    api.at("/take").post(take_message);
-    api
+fn add_messaging_endpoints(mut route: Route<QueueHub>) {
+    route.at("/:queue_name/push").post(push_message);
+    route.at("/:queue_name/take").post(take_message);
+}
+
+#[derive(Deserialize)]
+struct CreateQueue {
+    queue_name: String,
+}
+
+async fn create_queue(mut req: Req) -> Resp {
+    use crate::queue::CreateQueueResult::*;
+
+    let CreateQueue { queue_name } = req.body_json().await?;
+    let res = req.state().create_queue(&queue_name).await;
+    Ok(match res {
+        Done => Response::builder(StatusCode::Created),
+        AlreadyExists => {
+            Response::builder(StatusCode::Conflict).body("queue already exists")
+        }
+    }
+    .build())
+}
+
+async fn delete_queue(req: Req) -> TResult<StatusCode> {
+    use crate::queue::DeleteQueueResult::*;
+
+    let queue_name = req.param("queue_name")?;
+    let res = req.state().delete_queue(&queue_name).await;
+    match res {
+        Done => Ok(StatusCode::NoContent),
+        DoesNotExist => Ok(StatusCode::NotFound),
+    }
 }
 
 async fn reset_queue(req: Req) -> TResult<StatusCode> {
-    req.state().reset().await;
-    Ok(StatusCode::NoContent)
+    use crate::queue::ResetQueueResult::*;
+
+    let res = req.state().reset(req.param("queue_name")?).await;
+    match res {
+        Done => Ok(StatusCode::NoContent),
+        DoesNotExist => Ok(StatusCode::NotFound),
+    }
 }
 
-fn make_management_routes(state: Queue) -> Server<Queue> {
-    let mut api = tide::with_state(state);
-    api.at("/reset_queue").post(reset_queue);
-    api
+fn add_queue_endpoints(mut route: Route<QueueHub>) {
+    route.at("/create").post(create_queue);
+    route.at("/:queue_name").delete(delete_queue);
+    route.at("/:queue_name/reset").post(reset_queue);
+}
+
+#[derive(Deserialize)]
+struct StatsParams {
+    #[serde(default)]
+    queue_name_prefix: String,
 }
 
 #[derive(Serialize)]
 struct Stats {
-    size: usize,
+    size: HashMap<String, usize>,
 }
 
 async fn stats(req: Req) -> TResult<Body> {
     let q = req.state();
+    let params: StatsParams = req.query()?;
     let response = Stats {
-        size: q.size().await,
+        size: q.size(&params.queue_name_prefix).await,
     };
     Body::from_json(&response)
 }
 
 pub async fn start_http() -> IoResult<()> {
     tide::log::start();
-    let state = Queue::new();
+    let state = QueueHub::new();
     let mut app = tide::with_state(state.clone());
     app.at("/").get(|_| async { Ok("This is Flume") });
-    app.at("/messaging")
-        .nest(make_messaging_routes(state.clone()));
-    app.at("/management")
-        .nest(make_management_routes(state.clone()));
+    add_messaging_endpoints(app.at("/messaging"));
+    add_queue_endpoints(app.at("/queue"));
     app.at("/stats").get(stats);
     app.listen("127.0.0.1:8080").await?;
     Ok(())
--- src/queue.rs
+++ src/queue.rs
@@ -1,33 +1,126 @@
-use async_std::sync::{Arc, Mutex};
-use std::collections::VecDeque;
+use async_std::sync::{Arc, Mutex, RwLock};
+use patricia_tree::PatriciaMap;
+use std::collections::{HashMap, VecDeque};
 
 type Message = Vec<u8>;
 
+type Queues = RwLock<PatriciaMap<Mutex<VecDeque<Message>>>>;
+
 #[derive(Clone)]
-pub struct Queue {
-    q: Arc<Mutex<VecDeque<Message>>>,
+pub struct QueueHub {
+    queues: Arc<Queues>,
 }
 
-impl Queue {
+impl QueueHub {
     pub fn new() -> Self {
         Self {
-            q: Arc::new(Mutex::new(VecDeque::new())),
+            queues: Arc::new(RwLock::new(PatriciaMap::new())),
         }
     }
 
-    pub async fn push(&self, message: Message) {
-        self.q.lock().await.push_back(message)
+    pub async fn create_queue(&self, queue_name: &str) -> CreateQueueResult {
+        use CreateQueueResult::*;
+
+        let mut qs = self.queues.write().await;
+        if qs.contains_key(queue_name) {
+            AlreadyExists
+        } else {
+            qs.insert(queue_name, Mutex::new(VecDeque::new()));
+            Done
+        }
     }
 
-    pub async fn take(&self) -> Option<Message> {
-        self.q.lock().await.pop_front()
+    pub async fn delete_queue(&self, queue_name: &str) -> DeleteQueueResult {
+        use DeleteQueueResult::*;
+
+        let mut qs = self.queues.write().await;
+        if qs.contains_key(queue_name) {
+            qs.remove(queue_name);
+            Done
+        } else {
+            DoesNotExist
+        }
     }
 
-    pub async fn reset(&self) {
-        self.q.lock().await.clear()
+    pub async fn reset(&self, queue_name: &str) -> ResetQueueResult {
+        use ResetQueueResult::*;
+        if let Some(q) = self.queues.read().await.get(queue_name) {
+            q.lock().await.clear();
+            Done
+        } else {
+            DoesNotExist
+        }
     }
 
-    pub async fn size(&self) -> usize {
-        self.q.lock().await.len()
+    pub async fn push(
+        &self,
+        queue_name: &str,
+        message: Message,
+    ) -> PushMessageResult {
+        use PushMessageResult::*;
+
+        let qs = self.queues.read().await;
+        match qs.get(queue_name) {
+            Some(q) => {
+                q.lock().await.push_back(message);
+                Done
+            }
+            None => QueueDoesNotExist,
+        }
     }
+
+    pub async fn take(&self, queue_name: &str) -> TakeMessageResult {
+        use TakeMessageResult::*;
+
+        let qs = self.queues.read().await;
+        match qs.get(queue_name) {
+            Some(q) => match q.lock().await.pop_front() {
+                Some(payload) => Message { payload },
+                None => QueueIsEmpty,
+            },
+            None => QueueDoesNotExist,
+        }
+    }
+
+    pub async fn size(
+        &self,
+        queue_name_prefix: &str,
+    ) -> HashMap<String, usize> {
+        let mut res = HashMap::new();
+        let qs = self.queues.read().await;
+        let q_names = qs.iter_prefix(queue_name_prefix.as_bytes());
+        for (queue_name, queue) in q_names {
+            res.insert(
+                String::from_utf8(queue_name).unwrap(),
+                queue.lock().await.len(),
+            );
+        }
+        res
+    }
+}
+
+pub enum CreateQueueResult {
+    Done,
+    AlreadyExists,
+}
+
+pub enum DeleteQueueResult {
+    Done,
+    DoesNotExist,
+}
+
+pub enum ResetQueueResult {
+    Done,
+    DoesNotExist,
+}
+
+pub enum PushMessageResult {
+    Done,
+    QueueDoesNotExist,
+}
+
+pub enum TakeMessageResult {
+    Message { payload: Message },
+    QueueDoesNotExist,
+    QueueIsEmpty,
 }
